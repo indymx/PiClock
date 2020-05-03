b854e0d39236c0c22ac5bcbd424a16d9563905db
indymx <rXG2VsNegf8L>
2020-05-03 08:30:36 -0400

    ooops

--- Clock/ApiKeys-example.py
+++
@@ -1,9 +0,0 @@
-# change this to your API keys
-# DarkSky API key
-dsapi = '6878e51327eef5d33f5ca17d60187fce'
-
-# Map API keys -- only needs 1 of the following
-# Google Maps API key (if usemapbox is not set in Config)
-googleapi = 'AIzaSyD-bnjjrVA_7VFDRJKF6jQGoqCsaKhyxvM'
-# Mapbox API key (access_token) [if usemapbox is set in Config]
-mbapi = 'YOUR MAPBOX ACCESS TOKEN'
