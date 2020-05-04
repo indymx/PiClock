867d65bda1646a0c16b62b815368526df27e4f33
indymx <rXG2VsNegf8L>
2020-05-03 22:01:46 -0400

    update

--- Clock/PyQtPiClock.py
+++ Clock/PyQtPiClock.py
@@ -271,20 +271,20 @@ def wxfinished():
 # Config.LPrecip1hr + f['precip_1hr_metric'] + 'mm ' +
 # Config.LToday + f['precip_today_metric'] + 'mm')
     else:
-        temper.setText('%.1f' % (f['temp'] + u'°F')
-        temper2.setText('%.1f' % (f['temp'] + u'°F')
-        press.setText(Config.LPressure + '%.2f' % pressi(f[int('pressure')]) + 'in')
-        humidity.setText(Config.LHumidity + '%.0f%%' % (f[int('humidity')]*100.0))
-        wd = bearing(f['wind'][0][int('deg')])
+        temper.setText('%.1f' % (f['temp']) + u'°F')
+        temper2.setText('%.1f' % (f['temp']) + u'°F')
+        press.setText(Config.LPressure + '%.2f' % pressi(f[('pressure')]) + 'in')
+        humidity.setText(Config.LHumidity + '%.0f%%' % (f[('humidity')]*100.0))
+        wd = bearing(f['wind'][0][('deg')])
         if Config.wind_degrees:
-            wd = str(f['wind'][0][int('deg')]) + u'°'
+            wd = str(f['wind'][0][('deg')]) + u'°'
         wind.setText(Config.LWind +
                      wd + ' ' +
-                     '%.1f' % (f['wind'][0[int('speed')]]) + 'mph' +
+                     '%.1f' % (f['wind'][0[('speed')]]) + 'mph' +
                      Config.Lgusting +
                      '%.1f' % (f['windGust']) + 'mph')
         wind2.setText(Config.LFeelslike +
-                      '%.1f' % (f[int('feels_like')]) + u'°F')
+                      '%.1f' % (f[('feels_like')]) + u'°F')
         wdate.setText("{0:%H:%M}".format(datetime.datetime.fromtimestamp(
             int(f['time']))))
 # Config.LPrecip1hr + f['precip_1hr_in'] + 'in ' +
