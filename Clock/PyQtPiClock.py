0dc8047224c81627aad941d51e3c8135ff89ff76
indymx <rXG2VsNegf8L>
2020-05-03 21:54:16 -0400

    update

--- Clock/PyQtPiClock.py
+++ Clock/PyQtPiClock.py
@@ -271,8 +271,8 @@ def wxfinished():
 # Config.LPrecip1hr + f['precip_1hr_metric'] + 'mm ' +
 # Config.LToday + f['precip_today_metric'] + 'mm')
     else:
-        temper.setText('%.1f' % (f[int(temp)]) + u'°F')
-        temper2.setText('%.1f' % (f[int('temp')]) + u'°F')
+        temper.setText('%.1f' % int(f['temp']) + u'°F')
+        temper2.setText('%.1f' % int(f['temp']) + u'°F')
         press.setText(Config.LPressure + '%.2f' % pressi(f[int('pressure')]) + 'in')
         humidity.setText(Config.LHumidity + '%.0f%%' % (f[int('humidity')]*100.0))
         wd = bearing(f['wind'][0][int('deg')])
